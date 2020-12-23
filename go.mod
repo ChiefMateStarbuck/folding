0d04a53034d36e8f5306e13566e8b8464da49619
samuel <septimusguy@gmail.com>
2020-12-22 18:45:42 -0600

    Working Script

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ChiefMateStarbuck/folding
+
+go 1.15
+
+require github.com/digitalocean/godo v1.54.0
--- script.go
+++ script.go
@@ -1,9 +1,60 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/digitalocean/godo"
 )
 
 func main() {
-	fmt.Println("hello world!!")
+	fmt.Println("Creating kube cluster...")
+	client := godo.NewFromToken(os.Getenv("DO_TOKEN"))
+	ctx := context.TODO()
+	cluster := createCLuster(client, ctx)
+	deployFoldingImage(cluster.ID)
+	fmt.Println("Successfully created a folding cluster!")
+}
+
+// createCluster
+func createCLuster(client *godo.Client, ctx context.Context) *godo.KubernetesCluster {
+	createRequest := &godo.KubernetesClusterCreateRequest{
+		Name:        "folding-cluster",
+		RegionSlug:  "sfo2",
+		VersionSlug: "1.19.3-do.2",
+		Tags:        []string{"folding"},
+		NodePools: []*godo.KubernetesNodePoolCreateRequest{
+			{
+				Name:   "folding-pool",
+				Size:   "c-32",
+				Count:  5,
+				Tags:   []string{"folding"},
+				Labels: map[string]string{"app": "folding"},
+			},
+		},
+	}
+	cluster, _, err := client.Kubernetes.Create(ctx, createRequest)
+	handleError(err)
+	return cluster
+}
+
+// deployFoldingImage
+func deployFoldingImage(id string) {
+	fmt.Println("Waiting for the cluster to be provisioned...")
+	time.Sleep(4 * time.Minute)
+	cmd := exec.Command("doctl", "kubernetes", "cluster", "kubeconfig", "save", id)
+	err := cmd.Run()
+	handleError(err)
+	cmd = exec.Command("kubectl", "apply", "-f", "deployment.yml")
+	err = cmd.Run()
+	handleError(err)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
